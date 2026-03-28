79839ea4cefd33ea5dd4f01658e13c92999b499f
Tomekske <joostenstomek@gmail.com>
2026-03-28 16:14:27 +0100

    Move "Create New Folder" button from `GalleryView` to `NavigationPaneView`. Implement UI updates in XAML and refactor folder creation logic into `NavigationPaneViewModel`.

--- Main/ViewModels/GalleryViewModel.cs
+++ Main/ViewModels/GalleryViewModel.cs
@@ -1,48 +1,7 @@
-using System;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Mvvm.Messaging;
-using Graph.Domain.Interfaces;
-using Main.Messages;
-using Main.Views;
-using Serilog;
-using SukiUI.Dialogs;
-using SukiUI.Toasts;
-
 namespace Main.ViewModels;
 
 public partial class GalleryViewModel : ViewModelBase {
-    private readonly IFolderService _folderService;
-
-    public GalleryViewModel(IFolderService folderService) {
-        _folderService = folderService;
-    }
-
-    [RelayCommand]
-    public async Task OpenCreateFolderDialogAsync() {
-        var folders = await _folderService.FindAllAsync();
-
-        // 2. Initialize the ViewModel
-        var vm = new CreateFolderDialogViewModel(_folderService, result => {
-            if (result != null) {
-                // Refresh logic here
-                Log.Information("Folder created: {result}", result.Name);
-                RefreshGallery();
-
-                WeakReferenceMessenger.Default.Send(new FolderCreatedMessage(result));
-
-                MainWindow.ToastManager.CreateToast()
-                    .WithTitle("Success")
-                    .WithContent($"Folder '{result.Name}' has been created.")
-                    .Dismiss().After(TimeSpan.FromSeconds(3))
-                    .Queue();
-            }
-        }, folders);
-
-        // 3. Trigger SukiDialog
-        MainWindow.DialogManager.CreateDialog()
-            .WithContent(new CreateFolderDialog { DataContext = vm })
-            .TryShow();
+    public GalleryViewModel() {
     }
 
     private void RefreshGallery() {
--- Main/ViewModels/NavigationPaneViewModel.cs
+++ Main/ViewModels/NavigationPaneViewModel.cs
@@ -1,28 +1,60 @@
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using Avalonia.Threading;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Database.Domain.Entities;
 using Graph.Domain.Interfaces;
 using Main.Messages;
+using Main.Views;
+using SukiUI.Dialogs;
+using SukiUI.Toasts;
 
 namespace Main.ViewModels;
 
 public partial class NavigationPaneViewModel : ViewModelBase {
     private readonly INodeService _nodeService;
+    private readonly IFolderService _folderService;
 
     [ObservableProperty]
     private ObservableCollection<NavigationNodeViewModel> _folders = new();
 
-    public NavigationPaneViewModel(INodeService nodeService) {
+    public NavigationPaneViewModel(INodeService nodeService, IFolderService folderService) {
         _nodeService = nodeService;
+        _folderService = folderService;
         _ = LoadFoldersAsync();
 
         WeakReferenceMessenger.Default.Register<NavigationPaneViewModel, FolderCreatedMessage>(this,
             (r, m) => _ = r.LoadFoldersAsync());
     }
 
+    [RelayCommand]
+    public async Task OpenCreateFolderDialogAsync() {
+        var folders = await _folderService.FindAllAsync();
+
+        var vm = new CreateFolderDialogViewModel(_folderService, result => {
+            if (result != null) {
+                Serilog.Log.Information("Folder created: {result}", result.Name);
+                
+                // Broadcast creation to refresh the tree
+                WeakReferenceMessenger.Default.Send(new FolderCreatedMessage(result));
+
+                MainWindow.ToastManager.CreateToast()
+                    .WithTitle("Success")
+                    .WithContent($"Folder '{result.Name}' has been created.")
+                    .Dismiss().After(TimeSpan.FromSeconds(3))
+                    .Queue();
+            }
+        }, folders);
+
+        MainWindow.DialogManager.CreateDialog()
+            .WithContent(new CreateFolderDialog { DataContext = vm })
+            .TryShow();
+    }
 
     public void Receive(FolderCreatedMessage message) {
         // Refresh the navigation pane
