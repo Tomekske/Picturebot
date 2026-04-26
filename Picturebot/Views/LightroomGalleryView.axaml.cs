44a9d3be4b4011f36ac040443e8710d300100743
Tomekske <joostenstomek@gmail.com>
2026-04-26 18:46:29 +0200

    feat: add AspectRatioPanel, AspectRatioConverter, and LightroomGalleryView, update GalleryView, bump version to 1.1.7

---
+++ Picturebot/Views/LightroomGalleryView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace Picturebot.Views;
+
+public partial class LightroomGalleryView : UserControl {
+    public LightroomGalleryView() {
+        InitializeComponent();
+    }
+}
