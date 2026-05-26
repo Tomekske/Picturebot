0d284af5c1ba2d1e3fd9dbca2b41767345ef3088
Tomekske <joostenstomek@gmail.com>
2026-05-26 19:04:24 +0200

    feat: improve `GalleryView` star rating display with outlined stars and enhance `RatingToBooleanConverter` with negation support

--- Picturebot/Converters/RatingToBooleanConverter.cs
+++ Picturebot/Converters/RatingToBooleanConverter.cs
@@ -6,8 +6,14 @@ namespace Picturebot.Converters;
 
 public class RatingToBooleanConverter : IValueConverter {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-        if (value is int rating && parameter is string targetStr && int.TryParse(targetStr, out var targetRating)) {
-            return rating >= targetRating;
+        if (value is int rating && parameter is string paramStr) {
+            bool negate = paramStr.StartsWith("!");
+            string targetStr = negate ? paramStr.Substring(1) : paramStr;
+
+            if (int.TryParse(targetStr, out var targetRating)) {
+                bool result = rating >= targetRating;
+                return negate ? !result : result;
+            }
         }
         return false;
     }
